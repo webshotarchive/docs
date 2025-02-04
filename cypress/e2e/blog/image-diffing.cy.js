60d140726d50b6591e2b3c0197d28cf551f6bd0d
Toshi Moto <toshimoto821@proton.me>
2025-02-04 06:30:01 -0500

    fix: url for diffs

--- cypress/e2e/blog/image-diffing.cy.js
+++ cypress/e2e/blog/image-diffing.cy.js
@@ -2,7 +2,7 @@
 
 describe('Blog Post tags-[blog]', () => {
   it('image diffing', () => {
-    cy.visit('/blog/image-diffs');
+    cy.visit('/blog/diffs');
     cy.screenshot({
       capture: 'fullPage',
     });
