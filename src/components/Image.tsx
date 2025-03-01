606054386f88bfe535870d1b248af8740007c49f
Toshi Moto <toshimoto821@proton.me>
2025-03-01 15:53:51 -0500

    chore: log

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,5 +4,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export default function Image(props: { src: string; alt: string }) {
   const { siteConfig } = useDocusaurusContext();
   const ci = siteConfig.customFields.ci;
+  console.log(siteConfig);
   return <img {...props} loading={ci ? 'eager' : 'lazy'} />;
 }
