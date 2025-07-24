f34fa13e93ced36ebaef128f193c60f32755ef37
Toshi Moto <toshimoto821@proton.me>
2025-07-24 06:21:45 -0400

    chore: updates social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/webshot-archive-social-card.jpg',
     navbar: {
       title: 'Webshot Archive Docs',
       logo: {
