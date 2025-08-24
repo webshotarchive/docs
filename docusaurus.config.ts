7f882f9fca2e847a5b14c16fb082cf3eb6afd82e
Toshi Moto <toshimoto821@proton.me>
2025-08-24 16:33:44 -0400

    chore: updates links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -92,7 +92,7 @@ const config: Config = {
         { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://www.webshotarchive.com',
-          label: 'Website',
+          label: 'Webshot Archive',
           position: 'right',
         },
         {
